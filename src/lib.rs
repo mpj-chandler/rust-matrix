0753764a620a8efa9f11178e79875dc7106fb34b
Marcus Chandler <emailmarcuschandler@gmail.com>
2024-05-27 20:27:42 +0100

    mc: added basic test for initialiser

--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,25 @@ impl fmt::Display for Matrix {
 	fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
 		for row in &self.entries {
 			let printable_row: String = row.iter().map( |&entry| entry.to_string() + ", ").collect();
-			fmt.write_str(&printable_row)?;
-			fmt.write_str("\n");
+			let _ = fmt.write_str(&printable_row)?;
+			let _ = fmt.write_str("\n");
 		}
 		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_initialiser() {
+		let test_matrix = Matrix::new(4, 4);
+
+    	assert_eq!(test_matrix.entries.len(), 4);
+
+    	for row in test_matrix.entries {
+    		assert_eq!(row.len(), 4);
+    	}
 	}
 }
