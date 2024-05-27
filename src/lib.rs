2c63ffb1b1ebfad91fb72c5d22aab461d510216c
Marcus Chandler <emailmarcuschandler@gmail.com>
2024-05-27 20:48:25 +0100

    mc: added failing test for matrix_multiply

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,38 @@
 pub mod matrix_algebra {
-use std::fmt;
+	use std::fmt;
 
-pub struct Matrix {
-	pub entries: Vec<Vec<i32>>,
-}
+	pub struct Matrix {
+		pub entries: Vec<Vec<i32>>,
+	}
 
-impl Matrix {
-	pub fn new(n: usize, m: usize, value: i32) -> Matrix {
-		let row_vec = vec![value; n];
-		Matrix {
-			entries: vec![row_vec; m],
+	impl Matrix {
+		pub fn new(n: usize, m: usize, value: i32) -> Matrix {
+			let row_vec = vec![value; n];
+			Matrix {
+				entries: vec![row_vec; m],
+			}
 		}
 	}
-}
 
-impl fmt::Display for Matrix {
-	fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-		for row in &self.entries {
-			let printable_row: String = row.iter().map( |&entry| entry.to_string() + ", ").collect();
-			let _ = fmt.write_str(&printable_row)?;
-			let _ = fmt.write_str("\n");
+	impl fmt::Display for Matrix {
+		fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+			for row in &self.entries {
+				let printable_row: String = row.iter().map( |&entry| entry.to_string() + ", ").collect();
+				let _ = fmt.write_str(&printable_row)?;
+				let _ = fmt.write_str("\n");
+			}
+			Ok(())
 		}
-		Ok(())
 	}
-}
+
+	pub fn matrix_multiply(_a: Matrix, _b: Matrix) -> Matrix {
+		Matrix::new(4, 4, 0)
+	}
 }
 
 #[cfg(test)]
 mod tests {
+	use crate::matrix_algebra::matrix_multiply;
 	use crate::matrix_algebra::Matrix;
 
 	use rand::prelude::*;
@@ -44,5 +49,15 @@ mod tests {
     	for row in test_matrix.entries {
     		assert_eq!(row.len(),n);
     	}
+	}
+
+	#[test]
+	fn test_matrix_multiply() {
+		let test_matrix_a = Matrix::new(3, 4, 5);
+		let test_matrix_b = Matrix::new(5, 3, 4);
+
+		let matrix_product = matrix_multiply(test_matrix_a, test_matrix_b);
+
+		assert_eq!(matrix_product.entries.len(), 7);
 	}
 }
