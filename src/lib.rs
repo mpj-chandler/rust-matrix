34222c0ff3f22ca887b26f2f04ecd2a7123b2a58
Marcus Chandler <emailmarcuschandler@gmail.com>
2024-05-30 21:01:25 +0100

    mc: added failing test for panic on incorrect matrix dimensions

--- src/lib.rs
+++ src/lib.rs
@@ -21,9 +21,6 @@ impl fmt::Display for Matrix {
 		for row_index in 0..self.n {
 			let start_index = (row_index * self.m) as usize;
 			let end_index = (((row_index + 1) * self.m) - 1) as usize;
-			// let _ = fmt.write_str(&start_index.to_string())?;
-			// let _ = fmt.write_str(&end_index.to_string())?;
-
 			let printable_row: String = self.entries[start_index..=end_index].iter().map( |&entry| entry.to_string() + ", ").collect();
 			let _ = fmt.write_str(&printable_row)?;
 			let _ = fmt.write_str("\n");
@@ -38,8 +35,14 @@ mod tests {
 
 	#[test]
 	fn test_initialiser() {
-		let test_matrix = Matrix::new(4, 4, vec![1,16]);
+		let test_matrix = Matrix::new(4, 4, vec![1; 16]);
 
     	assert_eq!(test_matrix.entries.len(), 16);
+	}
+
+	#[test]
+	#[should_panic]
+	fn test_initialiser_panic() {
+		let _ = Matrix::new(4, 6, vec![1; 16]);
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use matrix::Matrix;
 
 fn main() {
-    let test_matrix = Matrix::new(4, 4, [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1].to_vec());
+    let test_matrix = Matrix::new(4, 4, vec![1; 16]);
 
     println!("{test_matrix}");
 }
