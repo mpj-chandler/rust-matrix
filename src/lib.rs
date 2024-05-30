dee3ea5a10647b03644e5642eefeb490c7e91575
Marcus Chandler <emailmarcuschandler@gmail.com>
2024-05-30 21:20:13 +0100

    mc: added new convenience initialiser for identity matrices

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,30 @@ pub struct Matrix {
 
 impl Matrix {
 	pub fn new(n: i32, m: i32, entries: Vec<i32>) -> Matrix {
+		if entries.len() != (n * m).try_into().unwrap() {
+			panic!("Matrix incorrectly specified! Number of entries should equal n * m");
+		}
+
 		Matrix {
 			n,
 			m,
+			entries,
+		}
+	}
+
+	pub fn new_identity_matrix(n: i32) -> Matrix {
+		let mut entries: Vec<i32> = Vec::new();
+		for row_index in 0..n {
+			let mut new_row = vec![0; n as usize];
+			new_row[row_index as usize] = 1;
+			for entry in new_row {
+				entries.push(entry);
+			}
+		}
+
+		Matrix {
+			n,
+			m: n,
 			entries,
 		}
 	}
@@ -19,8 +40,8 @@ impl Matrix {
 impl fmt::Display for Matrix {
 	fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
 		for row_index in 0..self.n {
-			let start_index = (row_index * self.m) as usize;
-			let end_index = (((row_index + 1) * self.m) - 1) as usize;
+			let start_index: usize = (row_index * self.m).try_into().unwrap();
+			let end_index: usize = (((row_index + 1) * self.m) - 1).try_into().unwrap();
 			let printable_row: String = self.entries[start_index..=end_index].iter().map( |&entry| entry.to_string() + ", ").collect();
 			let _ = fmt.write_str(&printable_row)?;
 			let _ = fmt.write_str("\n");
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use matrix::Matrix;
 
 fn main() {
-    let test_matrix = Matrix::new(4, 4, vec![1; 16]);
-
+    // let test_matrix = Matrix::new(4, 4, vec![1; 16]);
+    let test_matrix = Matrix::new_identity_matrix(4);
     println!("{test_matrix}");
 }
