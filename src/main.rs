9a94778e9b00647c89af6dd84284d8a777f6488e
Marcus Chandler <emailmarcuschandler@gmail.com>
2024-09-06 22:17:07 +0100

    mc: updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use matrix::Matrix;
+use matrix::matrix_algebra::Matrix;
 
 fn main() {
-    // let test_matrix = Matrix::new(4, 4, vec![1; 16]);
-    let test_matrix = Matrix::new_identity_matrix(4);
+    let test_matrix = Matrix::new(4, 4, 5);
+
     println!("{test_matrix}");
 }
