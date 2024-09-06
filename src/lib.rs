0a3c9d5931371de1a5bb1a29b16da3883155bc2b
Marcus Chandler <emailmarcuschandler@gmail.com>
2024-09-06 21:40:53 +0100

    Revert "Implemented base functionality up to matrix multiplication"

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,5 @@
-pub mod matrix_algebra {
-	use std::fmt;
+use std::fmt;
 
-<<<<<<< HEAD
-	#[derive(Clone)]
-	pub struct Matrix {
-		pub n: usize,
-		pub m: usize,
-		pub entries: Vec<Vec<i32>>,
-	}
-
-	impl Matrix {
-		pub fn new(m: usize, n: usize, value: i32) -> Matrix {
-			if m == 0 || n == 0 {
-				panic!("Matrix dimensions must each be greater than zero!");
-			}
-			let row_vec = vec![value; n];
-			Matrix {
-				n: n,
-				m: m,
-				entries: vec![row_vec; m],
-			}
-		}
-
-		pub fn columns(self) -> Vec<Vec<i32>> {
-			let n = self.n;
-			let m = self.m;
-
-			let mut columns = vec![vec![0; m]; n];
-			for row_index in 0..m {
-				for column_index in 0..n {
-					columns[column_index][row_index] = self.entries[row_index][column_index];
-				}
-			}
-
-			columns
-=======
 pub struct Matrix {
 	n: i32,
 	m: i32,
@@ -68,9 +33,9 @@ impl Matrix {
 			n,
 			m: n,
 			entries,
->>>>>>> main
 		}
 	}
+}
 
 impl fmt::Display for Matrix {
 	fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
@@ -81,90 +46,24 @@ impl fmt::Display for Matrix {
 			let _ = fmt.write_str(&printable_row)?;
 			let _ = fmt.write_str("\n");
 		}
-	}
-
-	fn is_multiplicatively_conformable(a: Matrix, b: Matrix) -> bool {
-		let n = a.entries[0].len();
-		let n_prime = b.entries.len();
-
-		n == n_prime
-	}
-
-	pub fn matrix_multiply(a: Matrix, b: Matrix) -> Matrix {
-		if !is_multiplicatively_conformable(a.clone(), b.clone()) {
-			panic!("Matrices are not multiplicatively conformable!");
-		}
-
-		let mut c = Matrix::new(b.entries[0].len(), a.entries.len(), 0);
-
-		for (i, a_row) in a.entries.iter().enumerate() {
-			for (k, a_ik) in a_row.iter().enumerate() {
-				for (j, b_kj) in b.entries[k].iter().enumerate() {
-					println!("C[{}{}] = A[{}{}] * B[{}{}]", i, j, i, k, k, j);
-					println!("C[{}{}] = {} * {}", i, j, a_ik, b_kj);
-					c.entries[i][j] = a_ik * b_kj;
-				}	
-			}
-		}
-		c
+		Ok(())
 	}
 }
 
 #[cfg(test)]
 mod tests {
-	use crate::matrix_algebra::matrix_multiply;
-	use crate::matrix_algebra::Matrix;
-
-	use rand::prelude::*;
+	use super::*;
 
 	#[test]
 	fn test_initialiser() {
-		let mut rng = rand::thread_rng();
-		let n = rng.gen_range(1..=100);
-		let m = rng.gen_range(1..=100);
-		let test_matrix = Matrix::new(m, n, 0);
+		let test_matrix = Matrix::new(4, 4, vec![1; 16]);
 
-    	assert_eq!(test_matrix.entries.len(), m);
-
-    	for row in test_matrix.entries {
-    		assert_eq!(row.len(),n);
-    	}
+    	assert_eq!(test_matrix.entries.len(), 16);
 	}
 
 	#[test]
 	#[should_panic]
-	fn test_zero_first_argument_to_initialiser() {
-		let _test_matrix = Matrix::new(0, 1, 1);
-	}
-
-	#[test]
-	#[should_panic]
-	fn test_zero_second_argument_to_initialiser() {
-		let _test_matrix = Matrix::new(1, 0, 1);
-	}
-
-	#[test]
-	#[should_panic]
-	fn test_panic_on_non_multiplicatively_conformable_matrices() {
-		let test_matrix_a = Matrix::new(3, 4, 5);
-		let test_matrix_b = Matrix::new(5, 7, 4);
-
-		matrix_multiply(test_matrix_a, test_matrix_b);
-	}
-
-	#[test]
-	fn test_matrix_multiply() {
-		let test_matrix_a = Matrix::new(3, 4, 5);
-		let test_matrix_b = Matrix::new(4, 3, 4);
-
-		println!("{} \n{}", test_matrix_a, test_matrix_b);
-
-		let matrix_product = matrix_multiply(test_matrix_a, test_matrix_b);
-
-		assert_eq!(matrix_product.entries.len(), 3);
-		
-		assert_eq!(matrix_product.entries[0], [20, 20, 20]);
-		assert_eq!(matrix_product.entries[1], [20, 20, 20]);
-		assert_eq!(matrix_product.entries[2], [20, 20, 20]);
+	fn test_initialiser_panic() {
+		let _ = Matrix::new(4, 6, vec![1; 16]);
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use matrix::matrix_algebra::Matrix;
+use matrix::Matrix;
 
 fn main() {
-<<<<<<< HEAD
-    let test_matrix = Matrix::new(4, 4, 5);
-
-=======
     // let test_matrix = Matrix::new(4, 4, vec![1; 16]);
     let test_matrix = Matrix::new_identity_matrix(4);
->>>>>>> main
     println!("{test_matrix}");
 }
