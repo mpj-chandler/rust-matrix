2b15ea4fe0daa79111b36bf28f7e58afff03d6f6
Marcus Chandler <emailmarcuschandler@gmail.com>
2024-11-13 12:39:50 +0000

    passing test for complex number multiplication and complex number subtraction

--- src/complex_number.rs
+++ src/complex_number.rs
@@ -1,7 +1,7 @@
 pub mod complex_number {
     use std::{
         fmt::Display,
-        ops::{Add, AddAssign, Mul},
+        ops::{Add, AddAssign, Mul, Sub},
     };
 
     #[derive(PartialEq, Debug)]
@@ -40,6 +40,49 @@ pub mod complex_number {
             complex_number_add(&self, &other)
         }
     }
+
+    fn complex_number_sub<
+        T: Add<Output = T> + Sub<Output = T> + AddAssign + Clone + Copy + Display + Mul + ?Sized,
+    >(
+        lhs: &ComplexNumber<T>,
+        rhs: &ComplexNumber<T>,
+    ) -> ComplexNumber<T> {
+        ComplexNumber {
+            real: lhs.real - rhs.real,
+            complex: lhs.complex - rhs.complex,
+        }
+    }
+
+    impl<T: Add<Output = T> + Sub<Output = T> + AddAssign + Clone + Copy + Display + Mul<Output = T> + ?Sized> Sub
+        for ComplexNumber<T>
+    {
+        type Output = Self;
+
+        fn sub(self, other: Self) -> Self {
+            complex_number_sub(&self, &other)
+        }
+    }
+
+    fn complex_number_multiply<T: Add<Output = T> + Sub<Output = T> + AddAssign + Clone + Copy + Display + Mul<Output = T> + ?Sized,
+    >(
+        lhs: &ComplexNumber<T>,
+        rhs: &ComplexNumber<T>,
+    ) -> ComplexNumber<T> {
+        ComplexNumber {
+            real: (lhs.real * rhs.real) - (lhs.complex * rhs.complex),
+            complex: (lhs.real * rhs.complex) + (rhs.real * lhs.complex),
+        }
+    }
+
+    impl<T: Add<Output = T> + Sub<Output = T> + AddAssign + Clone + Copy + Display + Mul<Output = T> + ?Sized> Mul
+        for ComplexNumber<T>
+    {
+        type Output = Self;
+
+        fn mul(self, rhs: Self) -> Self {
+            complex_number_multiply::<T>(&self, &rhs)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -59,5 +102,33 @@ mod tests {
         let rhs = ComplexNumber::new(real_two, complex_two);
 
         assert_eq!(lhs + rhs, ComplexNumber::new(real_one + real_two, complex_one + complex_two));
+    }
+
+    #[test]
+    fn test_complex_number_sub() {
+        let mut rng = rand::thread_rng();
+        let real_one = rng.gen_range(1..=100);
+        let complex_one = rng.gen_range(1..=100);
+        let real_two = rng.gen_range(1..=100);
+        let complex_two = rng.gen_range(1..=100);
+
+        let lhs = ComplexNumber::new(real_one, complex_one);
+        let rhs = ComplexNumber::new(real_two, complex_two);
+
+        assert_eq!(lhs - rhs, ComplexNumber::new(real_one - real_two, complex_one - complex_two));
+    }
+
+    #[test]
+    fn test_complex_number_multiply() {
+        let mut rng = rand::thread_rng();
+        let real_one = rng.gen_range(1..=100);
+        let complex_one = rng.gen_range(1..=100);
+        let real_two = rng.gen_range(1..=100);
+        let complex_two = rng.gen_range(1..=100);
+
+        let lhs = ComplexNumber::new(real_one, complex_one);
+        let rhs = ComplexNumber::new(real_two, complex_two);
+
+        assert_eq!(lhs * rhs, ComplexNumber::new((real_one * real_two) - (complex_one * complex_two), (complex_one * real_two) + (complex_two * real_one)));
     }
 }
