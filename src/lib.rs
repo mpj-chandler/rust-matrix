6f2cff1d65dab3f57d4e7ca50106668aea1df038
Marcus Chandler <emailmarcuschandler@gmail.com>
2025-02-15 21:41:52 +0000

    Started documentation
    Added a title page for the documentation, added /doc directory to .gitignore

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+//! This crate is a Rust implementation of some foundational matrix algebra
+//! functionality.
+//!
+//! While technically generic, the matrix library is only implemented for
+//! the <f32> and <f64> data types, as well as complex numbers (with real and
+//! imaginary components of the same <f32> / <f64> types), for which I have
+//! written a dedicated module.
+//!
+//! Users could, for instance make this library work for {integer} types,
+//! although there's not a huge amount of point as you would soon get into
+//! rounding issues.
+//!
+
 pub mod complex_number;
 pub mod matrix_algebra;
 pub mod pow;
